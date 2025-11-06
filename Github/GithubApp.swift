8e28427a205b8b2f7db7c2335e0d1723df5cd68f
Julie <tianaow@gmail.com>
2025-11-05 16:49:29 -0800

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by patron on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
